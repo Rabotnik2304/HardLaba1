325006b93b66f2e6d3d32b7a13a0fd75d514fabf
informant <artemiy.solodkin@mail.ru>
2022-11-20 19:22:56 +0500

    HardLaba1Finished

--- HardLaba1/Program.cs
+++ HardLaba1/Program.cs
@@ -1,10 +1,74 @@
-﻿namespace HardLaba1
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace HardLaba1
 {
-    internal class Program
+    public class Reader
     {
-        static void Main(string[] args)
+        public uint Id;
+        public string FullName;
+    }
+    public class Book
+    {
+        public uint Id;
+        public string Author;
+        public string Name;
+        public DateTime PublicationDate;
+        public uint BookcaseNumber;
+        public uint ShelfNumber;
+    }
+    public class Statistic
+    {
+        public uint Id;
+        public Reader Reader;
+        public Book Book;
+        public DateTime TakeDate;
+        public DateTime ReturnDate;
+    }
+    public class Program
+    {
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            var cultureInfo = new CultureInfo("ru-RU", false);
+
+            Reader reader1 = new Reader{ Id = 1,FullName="Петров Петр Иванович"};
+            Reader reader2 = new Reader { Id = 2, FullName = "Иванов Иван Петрович"};
+
+            Book book1 = new Book
+            {
+                Id = 1,
+                Author = "Алесандр Сергеевич Пушкин",
+                Name = "Борис Годунов",
+                PublicationDate = DateTime.Parse("12.06.1831", cultureInfo),
+                BookcaseNumber = 12,
+                ShelfNumber = 38
+            };
+            Book book2 = new Book
+            {
+                Id = 2,
+                Author = "Лев Николаевич Толстой",
+                Name = "Война и Мир",
+                PublicationDate = DateTime.Parse("16.09.1867", cultureInfo),
+                BookcaseNumber = 162,
+                ShelfNumber = 56
+            };
+
+            Statistic Statistic1 = new Statistic
+            {   
+                Id=1,
+                Reader=reader1,
+                Book =book1,
+                TakeDate = DateTime.Parse("16.09.2007", cultureInfo),
+                ReturnDate = DateTime.Parse("16.10.2007", cultureInfo),
+            };
+            Statistic Statistic2 = new Statistic
+            {
+                Id = 2,
+                Reader = reader2,
+                Book = book2,
+                TakeDate = DateTime.Parse("17.11.2009", cultureInfo),
+                ReturnDate = DateTime.Parse("19.12.2009", cultureInfo),
+            };
         }
     }
 }
